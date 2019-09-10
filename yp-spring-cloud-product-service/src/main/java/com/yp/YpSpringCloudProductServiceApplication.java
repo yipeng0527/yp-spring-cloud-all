f0b9b65d66ef4ed0648264022585695d2419fa7b
ex-yipeng <ex-yipeng@htlic.com>
2019-09-10 11:13:45 +0800

    新增数据库连接池

--- yp-spring-cloud-product-service/src/main/java/com/yp/YpSpringCloudProductServiceApplication.java
+++ yp-spring-cloud-product-service/src/main/java/com/yp/YpSpringCloudProductServiceApplication.java
@@ -1,9 +1,11 @@
 package com.yp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@MapperScan("com.yp.product.dao")
 @EnableDiscoveryClient
 @SpringBootApplication
 public class YpSpringCloudProductServiceApplication {
--- yp-spring-cloud-product-service/src/main/java/com/yp/product/dao/ProductDao.java
+++ yp-spring-cloud-product-service/src/main/java/com/yp/product/dao/ProductDao.java
@@ -1,5 +1,22 @@
 package com.yp.product.dao;
 
+import com.yp.vo.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 public interface ProductDao {
 
+    /**
+     * 查询所有产品
+     * @return
+     */
+    List<Product> queryAll();
+
+    /**
+     * 根据产品编码查询产品信息
+     * @param itemCode
+     * @return
+     */
+    Product getProductDetail(String itemCode);
 }
